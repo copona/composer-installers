ece5b2000b66051f6a419295debc22d10b868c6f
arnisjuraga <arnisjuraga@gmail.com>
2021-06-04 15:49:06 +0300

    additional abstract methods for composer 2

--- src/Copona/ComposerInstallers/CoponaCoreInstallerPlugin.php
+++ src/Copona/ComposerInstallers/CoponaCoreInstallerPlugin.php
@@ -13,4 +13,13 @@ class CoponaCoreInstallerPlugin implements PluginInterface
         $installer = new CoponaCoreInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
 }
